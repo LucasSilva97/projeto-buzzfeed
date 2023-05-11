f05cd8fff78adaf8e72148a8f5963558848206a0
Lucas Santana <lucas-silva-santana@hotmail.com>
2023-05-10 22:11:40 -0300

    calculating the result

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -40,6 +40,7 @@ export class QuizzComponent {
 
   playerChoose(value:string){
     this.answers.push(value)
+    this.nextStep()
     console.log(this.answers)
   }
 
@@ -49,7 +50,24 @@ export class QuizzComponent {
     if(this.questionMaxIndex > this.questionIndex){
       this.questionSelected = this.questions[this.questionIndex]
     }else{
+      const finalAnswer:string = await this.checkResult(this.answers)
       this.finished = true
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof
+      typeof quizz_questions.results]
     }
+  }
+
+  async checkResult(answers:string[]){
+    const result = answers.reduce((previous, current, index, arr)=>{
+      if(
+        arr.filter((item)=> item === previous).length >
+        arr.filter((item)=> item === current).length
+        ){
+          return previous
+      }else{
+          return current
+      }
+    })
+    return result
   }
 }
